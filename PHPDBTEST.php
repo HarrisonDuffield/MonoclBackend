cea11bb822731692ca79814d82cde89c9c741a02
UK1aO13CV <hpd12@live.co.uk>
2019-01-25 11:47:18 +0000

    Tidying up
    Test to try nad get working - web server now needed

--- PHPDBTEST.php
+++ PHPDBTEST.php
@@ -1,32 +1,45 @@
 <html>
+<link href="Assets/HomePageAssets.css" rel="stylesheet" type="text/css">
+<link href="Assets/FontAssets.css" rel="stylesheet" type="text/css">
+<link href="Assets/CommonAssets.css" rel="stylesheet" type="text/css">
+<div id="MonocleHeaderBar">
     <body>
-        <a> test</a>
+        <a> Registration Complete</a>
 <?php
 $servername = "localhost";
-$username = "PHPConnection2";
+$account = "PHPConnection2";
 $dbname = "monoclmain";
+$password="PHPPassword12";
 
-$ConnectionFunction = mysqli_connect($servername, $username, $password);
+
+$UserName =$_POST['UserName'];
+$FirstName = $_POST['FirstName'];
+$LastName = $_POST['LastName'];
+$EmailAddress = $_POST['Email'];
+$ClassCode = $_POST['ClassCode'];
+
+        
+$ConnectionFunction = mysqli_connect($servername,$account, $password);
 if(!$ConnectionFunction){
     die("Connection Failed");
-}
+    }
 echo "Console Message : Connection Succeeded";
-$UserName = mysqli_real_escape_string($ConnectionFunction,$_POST['UserName']);        
-$FirstName = mysqli_real_escape_string($ConnectionFunction,$_POST['FirstName']);
-$LastName = mysqli_real_escape_string($ConnectionFunction,$_POST['LastName']);
-$EmailAddress= mysqli_real_escape_string($ConnectionFunction,$_POST['Email']);
-$ClassCode= mysqli_real_escape_string($ConnectionFunction,$_POST['ClassCode']);
+$UserName = mysqli_real_escape_string($ConnectionFunction,$UserName);        
+$FirstName = mysqli_real_escape_string($ConnectionFunction,$FirstName);
+$LastName = mysqli_real_escape_string($ConnectionFunction,$LastName);
+$EmailAddress= mysqli_real_escape_string($ConnectionFunction,$Email);
+$ClassCode= mysqli_real_escape_string($ConnectionFunction,$ClassCode);
 $PasswordOriginal = $_POST['PwdInput'];
 $PasswordConfirmation = $_POST['PwdConfirmation'];
-echo $UserName;
 $querypublicdetails = "INSERT INTO userdetails (UserName,ClassID) VALUES ($UserName,$ClassCode)";
-$queryprivatedetails ="INSERT INTO userprivatedetails (FirstName,LastName,ClassID,PasswordHash,EmailAdress) VALUES ($FirstName,$LastName,$EmailAddress,$ClassCode,$PasswordOriginal)";
+$queryprivatedetails ="INSERT INTO userprivatedetails (FirstName,LastName,ClassID,PasswordHash,EmailAdress) VALUES ($FirstName,$LastName,$ClassCode,$PasswordOrignial,$EmailAddressl)";
         
-$test = mysqli_query($ConnectionFunction,$querypublicdetails);
-if($test){
+$result = mysqli_query($ConnectionFunction,$queryprivatedetails);
+if($result){
     echo "success";
 }
-      
+mysqli_close($ConnectionFunction);
 ?>
+        <a> Test</a>  
 </body>
 </html>
