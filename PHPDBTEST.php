6b9c32ad943105d5a2986064b723bbb237e59771
UK1aO13CV <hpd12@live.co.uk>
2019-01-29 20:09:49 +0000

    Working Iteration
    User is able to be inputted into the database
    
    Req's to be solved still  -
    echo statemetns still appear from testing
    tidying up on the html to improve looks but still establoishing functionality
    error system
    hide teh inputs with that dotted inputs that sites do
    redirect outwards
    
    proof of concept of the functionality into the mysql is demosntrated however

--- PHPDBTEST.php
+++ PHPDBTEST.php
@@ -9,11 +9,8 @@ $password="PHPPassword12";
 $validation=array();
 
         
-$ConnectionFunction = mysqli_connect($servername,$account, $password);
+$ConnectionFunction = mysqli_connect($servername, $account, $password, $dbname);
 
-$ConnectionTesting = mysqli_get_server_info($ConncetionFunction);
-
-echo $ConnectionTesting;
 if(!$ConnectionFunction){
     die("Connection Failed");
     
@@ -26,71 +23,118 @@ $EmailAddress="";
 $ClassCode="";
 $PasswordOriginal="";
 $PasswordConfirmation="";
-if(isset($_POST['SignUpButtonGreen'])){
+$UserIDToInsert="";
 
 $UserName = mysqli_real_escape_string($ConnectionFunction,$_POST['UserName']);
-
 $FirstName = mysqli_real_escape_string($ConnectionFunction,$_POST['FirstName']);
 $LastName = mysqli_real_escape_string($ConnectionFunction,$_POST['LastName']);
 $EmailAddress= mysqli_real_escape_string($ConnectionFunction,$_POST['Email']);
 $ClassCode= mysqli_real_escape_string($ConnectionFunction,$_POST['ClassCode']);
 $PasswordOriginal = mysqli_real_escape_string($ConnectionFunction,$_POST['PwdInput']);
 $PasswordConfirmation = mysqli_real_escape_string($ConnectionFunction,$_POST['PwdConfirmation']);
+
 if(empty($UserName)){
     array_push($validation,"Empty username");
+    unset($_POST['SignUpButtonGreen']);
 }
 if(empty($LastName)){
     array_push($validation,"Empty LastName");
+    unset($_POST['SignUpButtonGreen']);
 }
 if(empty($FirstName)){
     array_push($validation,"Empty FirstName");
+    unset($_POST['SignUpButtonGreen']);
 }
 if(empty($EmailAddress)){
     array_push($validation,"Empty Email");
+    unset($_POST['SignUpButtonGreen']);
 }
 if(empty($PasswordOriginal)){
     array_push($validation,"Password Field is empty");
+    unset($_POST['SignUpButtonGreen']);
 }
 if($PasswordOriginal != $PasswordConfirmation){
     array_push($validation,"Passwords don't match");
+    unset($_POST['SignUpButtonGreen']);
 }
-$UserNameCheck = "SELECT * FROM userdetails WHERE (UserName) = 'Admin' LIMIT 1";
+$UserNameCheck = "SELECT * FROM userdetails WHERE (UserName) = '$UserName' LIMIT 1";
 $EmailCheck = "SELECT * FROM userprivatedetails WHERE EmailAddress = '$EmailAddress' LIMIT 1";
 $UserNameCheckResult = mysqli_query($ConnectionFunction, $UserNameCheck);
+$EmailCheckResult = mysqli_query($ConnectionFunction, $EmailCheck);
 if($UserNameCheckResult==False){
-    array_push($validation,"Failure to see if username already there");
+    echo "Error Technical : Failure to access DB";
     
 }
 else{
     foreach($UserNameCheckResult as $row){
         echo $row['UserName'];
+        if ($row['UserName'] == $UserName){
+            echo "UserName Matches";
+            array_push($validation,"User Name already taken");
+        }
     }
 }
 
+if($EmailCheckResult==False){
+    echo "Error Technical : Failure to access DB";
+    
+}
+else{
+    foreach($EmailCheckResult as $row){
+        echo $row['EmailAddress'];
+        if ($row['EmailAddress'] == $EmailAddress){
+            
+            array_push($validation,"Account Already Exists with this email");
+        }
+    }
+}
+while(isset($_POST['SignUpButtonGreen'])&& (count($validation)<1)){
 if(empty($ClassCode)){
     $ClassCode=404;
 }
-
 if(count($validation)==0){
-    echo "count is 0";
-    $CreationPublicQuery = "INSERT INTO userdetails (UserName,ClassID) VALUES ($UserName,$ClassCode)";
-    $CreationPrivateQuery ="INSERT INTO userprivatedetails (FirstName,LastName,ClassID,PasswordHash,EmailAddress) VALUES ($FirstName,$LastName,$ClassCode,$PasswordOriginal,$EmailAddress)";
-    $result = mysqli_query($ConnectionFunction,$CreationPublicQuery);
-    $result2 = mysqli_query($ConnectionFunction,$CreationPrivateQuery);
-    if($result){
-        echo "Query Success";
+    $PasswordHashed = password_hash($PasswordOriginal,PASSWORD_DEFAULT);
+    echo $PasswordHashed;
+    
+    
+    $CreationPrivateQuery ="INSERT INTO userprivatedetails (FirstName,LastName,ClassID,PasswordHash,EmailAddress) VALUES ('$FirstName','$LastName','$ClassCode','$PasswordHashed','$EmailAddress')";
+    $PrivateQueryExecution = mysqli_query($ConnectionFunction,$CreationPrivateQuery);
+    
+    if($PrivateQueryExecution){
+        echo "  Private Query Success   ";
     }
-    if(!$result){
-        echo "Query Failure";
+    if(!$PrivateQueryExecution){
+        echo "  Private Query Failure   ";
     }
-    if($result2){
-        echo "Query Success";
+    
+    
+    
+    $UserIDEmailCheck = "SELECT * FROM userprivatedetails WHERE (EmailAddress) = '$EmailAddress' LIMIT 1";
+    $UserIDEmailCheckResult= mysqli_query($ConnectionFunction,$UserIDEmailCheck);
+    if($UserIDEmailCheckResult==False){
+    echo "Error Technical : Failure to access DB";
+    
     }
-    if(!$result2){
-        echo "Query Failure";
+    else{
+    foreach($UserIDEmailCheckResult as $row){
+        $UserIDToInsert=$row['UserID'];
     }
+    }
+    
+    $CreationPublicQuery = "INSERT INTO userdetails (UserID,UserName,ClassID) VALUES ('$UserIDToInsert','$UserName','$ClassCode')";
+    $PublicQueryExecution= mysqli_query($ConnectionFunction,$CreationPublicQuery);
+    echo $CreationPublicQuery;
+    
+    if($PublicQueryExecution){
+        echo "  Public Query Success  ";
+    }
+    if(!$PublicQueryExecution){
+        echo "  Public Query Failure    ";
+    }
+    break;
 }
 }
+
 
 ?>
 
