cd1208e91aac0d8e07e77e78e2896d85aabab95c
MonocledMajesticProductions <hpd12@live.co.uk>
2019-02-05 15:19:01 +0000

    Update EmailVerification.php
    Added function to chage the verified value in the table

--- EmailVerification.php
+++ EmailVerification.php
@@ -4,6 +4,11 @@ use PHPMailer\PHPMailer\Exception;
 require ('PHPMailer-6.0.7\PHPMailer-6.0.7\src\PHPMailer.php');
 require ('PHPMailer-6.0.7\PHPMailer-6.0.7\src\Exception.php');
 require ('PHPMailer-6.0.7\PHPMailer-6.0.7\src\SMTP.php');
+$servername = "localhost:3306";
+$account = "PHPConnection2";
+$dbname = "monoclmain";
+$password="PHPPassword12";
+$ConnectionFunction = mysqli_connect($servername, $account, $password, $dbname);
 function SendEmail($Destination,$EmailVerificationHash){
 $MailFunction = new PHPMailer(true);
 $MailFunction ->Username ="monoclnoreply@gmail.com";
@@ -17,12 +22,24 @@ $MailFunction->SMTPSecure="ssl";
 $MailFunction ->From = $MailFunction ->Username;
 $MailFunction ->AddAddress($Destination);
 $MailFunction ->Subject =" Monocl Account Verification";
-$MailFunction ->Body = "You have successfully "; 
+$MailFunction ->Body = "You have successfully activated your account please click the link to go"
+        . "activate your account "
+        . "http://127.0.0.1/AccountVerified.php?VerifyString='$EmailVerificationHash'"; 
 if($MailFunction->Send()){
     return true;       
     }
 else{
     return false;     
+    }
+}
+function TableUpdate($Hash){
+    $TableUpdateQuery = "UPDATE userdetails SET AccountVerified = 1 WHERE EmailVerificationHash = $Hash";
+    $TableUpdateExecution = mysqli_query($ConnectionFunction,$TableUpdateQuery);
+    if($TableUpdateExecution){
+        return true;
+    }
+    else{
+        return false;
     }
 }
 //}
