096be087e7de26291a59fcfe115c539d2b78953b
MonocledMajesticProductions <hpd12@live.co.uk>
2019-02-06 10:51:51 +0000

    Update QuestionTablePopulator.php

--- QuestionTablePopulator.php
+++ QuestionTablePopulator.php
@@ -1,7 +1,8 @@
  <?php
        $TopicDisplayArray = array();
        $QuestionDisplayArray = array();
-       $AnswerPercentage = array();
+       $AnswerPercentageArray = array();
+       $AnswerListArray = array();
        session_start();
        $servername = "localhost:3306";
        $account = "PHPConnection2";
@@ -18,7 +19,7 @@
                        
                    }
                    else{
-                       array_push($QuestionDisplayArray,$row['QuestioID']);
+                       array_push($QuestionDisplayArray,$row['QuestionID']);
                    }
                }
            }
@@ -27,7 +28,7 @@
        }
        }
        function TopicListRetreival(){
-       $TopicListQuery = "SELECT Topic from QuestionTable";
+       $TopicListQuery = "SELECT Topic FROM QuestionTable";
        $TopicListExecution= mysqli_query($ConnectionFunction, $TopicListQuery);
        if($TopicListExecution){
            foreach($TopicListExecution as $row){
@@ -45,19 +46,72 @@
             echo "Connection Error";
         }
        }
+       function AnswerListRetreival($UserIDToUse,$QuestionID){
+           $AnswerListQuery = "SELECT * FROM answertable WHERE UserID = '$UserIDToUse', QuestionID = '$QuestionID'";
+           $AnswerListExecution =mysqli_query($ConnectionFunction,$AnswerListQuery);
+           if($AnswerListExecution){
+               foreach($AnswerListExecution as $row){
+                   if(in_array($row['AnswerID'],$AnswerListArray())){
+                       echo "item already in the array";
+                       
+                   }
+                   else{
+                       array_push($AnswerListArray,$row['AnswerID']);
+                   }
+               }
+           }
+           else{
+               echo "Connection Failure";
+       }
+           
+           
+       }
        function AnsweredPercentageRetrieval(){
+           TopicListRetrieval();
+           $NumeratorOfAnsweredQuestionsInTopic = 0;
+           $DenominatorOfTotalQuestionsInTopic = 0;
            $UserIDToUse = $_SESSION["UserLoggedIn"];
-           $ArrayPercentageGatherQuery =
+           for($i =0;$i<count($TopicDisplayArray);$i++){
+               $NumeratorOfAnsweredQuestionsInTopic = 0;
+               $DenominatorOfTotalQuestionsInTopic = 0;
+               QuestionDisplayArray($TopicDisplayArray[$i]);
+               foreach($QuestionDisplayArray as $Questions){
+                   AnswerListRetrieval($Questions,$UserIDToUse);
+                   if(count($AnswerListArray>=1)){
+                       $NumeratorOfAnsweredQuestionsInTopic++;
+                       $DenominatorOfTotalQuestionsInTopic++;
+                       unset($AnswerListArray);
+                       $AnswerListArray = array();
+                   }
+                   else{
+                      $DenominatorOfTotalQuestionsInTopic++;
+                      unset($AnswerListArray);
+                      $AnswerListArray = array();
+                   }
+                                
+            }
+            unset($QuestionDisplayArray);
+            $QuestionDisplayArray = array();
+           $PercentageToAdd = ($NumeratorOfAnsweredQuestionsInTopic/$DenominatorOfTotalQuestionsInTopic);
+           array_push($AnswerPercentageArray,$PercentageToAdd);
+           //order of operation topics gathered , questions in each topic gathered,
+           // for each question see if the user has answered, answered/total questions = percentage for each topic
+       }
        }
        function TopicTableOrginisation(){
            echo "<Table>";
            echo "<tr>";
            echo "<th> Topic </th>";
+           AnsweredPercentageRetrieval();
            echo "<th> Answer Percentage</th>";
            echo "</tr>";
-           
-           
-           echo "</Table>";
+           for($e=0;$e<(count($TopicDisplayArray));$e++){
+               echo "<tr>";
+               echo "<td> '$TopicDisplayArray[$e]' </td>";
+               echo "<td> '$AnswerPercentageArray[$e]'</td>";
+               echo "</tr>";               
+           }
+          echo "</Table>";
        }
        
 ?>
