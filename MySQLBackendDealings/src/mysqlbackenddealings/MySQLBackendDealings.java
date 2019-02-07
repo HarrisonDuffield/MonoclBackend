17cf204d71feedd1414e3f2aba5ca6ba7fe198dd
MonocledMajesticProductions <hpd12@live.co.uk>
2019-02-07 19:45:53 +0000

    Update MySQLBackendDealings.java

--- MySQLBackendDealings/src/mysqlbackenddealings/MySQLBackendDealings.java
+++ MySQLBackendDealings/src/mysqlbackenddealings/MySQLBackendDealings.java
@@ -1,6 +1,10 @@
 package mysqlbackenddealings;
-
+import java.sql.*;
 public class MySQLBackendDealings {
+    public static String ConnectionLocation = "jdbc:mysql://localhost:3306/monoclmain";
+    public static String UserName ="JavaConnection";
+    public static String Password ="JavaPassword";
+    public static String Driver = "com.mysql.jdbc.Driver";
     public static void main(String[] args) {
         /**order of operation:
         Connects to DB
@@ -9,11 +13,18 @@ public class MySQLBackendDealings {
         Compares last answe rfor question to last update to that questions file - creates file if one not present
         Gets all the answers for that question
         does the keyword procedure*/
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection ConnectionFunction = DriverManager.getConncetion("jdbc:mysql://localhost:3306/monoclmain,PHPConnection2,PHPPassword12);
+        try{
+        Connection ConnectionFunction = DriverManager.getConnection(ConnectionLocation,UserName,Password);
         Statement test = ConnectionFunction.createStatement();
-
-
-    }
-
+        ResultSet results = test.executeQuery("Select * from answertable");       
+        System.out.println(results[2]);
+        ConnectionFunction.close();
+        System.out.println("Congratulations");
+        }
+        catch(Exception c){
+            System.out.println(c);
+        }
+               
+        }
+        
 }
