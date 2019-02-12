5d5b7e31d27791860dfec4b7dc3766dbca6c8461
MonocledMajesticProductions <hpd12@live.co.uk>
2019-02-12 18:25:11 +0000

    Switched to database approach
    Databse intreaction kept within sperate class -accessed from within the other class that does the handling of the data,keeps the code cleaner
    doesnt work

---
+++ MySQLBackendDealings/src/mysqlbackenddealings/DatabaseFunctions.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mysqlbackenddealings;
+
+import java.sql.*;
+
+/**
+ *
+ * @author hpd12
+ */
+public class DatabaseFunctions {
+    
+    public static String ConnectionLocation = "jdbc:mysql://localhost:3306/monoclmain";
+    public static String ConnectionLocationSecondaryTable="jdbc;mysql://localhost:3306/monoclquestionanswers";
+    public static String UserName ="JavaConnection";
+    public static String Password ="JavaPassword";
+    public static String Driver = "com.mysql.jdbc.Driver";
+    
+    
+    public static ResultSet IsTablePresent(String QuestionID){
+        try{
+        Connection ConnectionFunction = DriverManager.getConnection(ConnectionLocationSecondaryTable,UserName,Password);
+        ResultSet QueryToReturn = ConnectionFunction.getMetaData().getTables(null,null,QuestionID,null);
+        return QueryToReturn;
+        }
+        catch (Exception Failure){
+            Failure.printStackTrace();
+            return null;
+            
+        }
+    }
+    public static ResultSet SearchQueryReturn(String Query){
+        try{
+        Connection ConnectionFunction = DriverManager.getConnection(ConnectionLocation,UserName,Password);
+        Statement statement = ConnectionFunction.createStatement();
+        ResultSet QueryToReturn = statement.executeQuery(Query);
+        return QueryToReturn;
+        }
+        catch(Exception FalseSQLQueryResults){
+            FalseSQLQueryResults.printStackTrace();
+            return null;
+        }
+    }
+    public static Boolean ClearTable(String QuestionID){
+        try{
+        Connection ConnectionFunction = DriverManager.getConnection(ConnectionLocationSecondaryTable,UserName,Password);
+        Statement statement = ConnectionFunction.createStatement();
+        statement.execute("DROP TABLE "+QuestionID+";");
+        CreateTable(QuestionID);
+        return true;
+    }
+        catch(Exception DeleteFailureQuery){
+            DeleteFailureQuery.printStackTrace();
+            return false;
+        }
+    }
+    public static Boolean CreateTable(String QuestionID){
+        String CreationQuery="\n" +
+"CREATE TABLE `monoclquestionanswers`.`"+QuestionID+"` (\n" +
+"`AnswerWordId` INT NOT NULL AUTO_INCREMENT ,\n" +
+"`PreviousWord` LONGTEXT NULL DEFAULT NULL ,\n" +
+"`MainWord` LONGTEXT NOT NULL ,\n" +
+"`FollowingWord` LONGTEXT NULL DEFAULT NULL , \n" +
+"`Count` INT NOT NULL DEFAULT '0' ,\n" +
+"`Percentage` DECIMAL NULL DEFAULT NULL )\n" +
+"ENGINE = InnoDB;";
+        try{
+        Connection ConnectionFunction = DriverManager.getConnection(ConnectionLocationSecondaryTable,UserName,Password);
+        Statement statement = ConnectionFunction.createStatement();
+        statement.executeQuery(CreationQuery);
+        return true;
+        }
+        catch(Exception FalseSQLQueryResults){
+            FalseSQLQueryResults.printStackTrace();
+            return false;
+        }
+    }
+    public static boolean InsertData(String QuestionID,String Query){
+        try{
+        Connection ConnectionFunction = DriverManager.getConnection(ConnectionLocation,UserName,Password);
+        Statement statement = ConnectionFunction.createStatement();
+        statement.executeUpdate(Query);
+        ResultSet QueryToReturn = ConnectionFunction.getMetaData().getTables(null,null,QuestionID,null);
+        return true;
+        }
+        catch(Exception Insert){
+            Insert.printStackTrace();
+            return false;
+        }
+    }
+    
+}
--- MySQLBackendDealings/src/mysqlbackenddealings/MySQLBackend.java
+++ MySQLBackendDealings/src/mysqlbackenddealings/MySQLBackend.java
@@ -1,20 +1,8 @@
-package mysqlbackend;
-import java.io.File;
-import java.io.FileWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+package mysqlbackenddealings;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import mysqlbackenddealings.TableItem;
+import static mysqlbackenddealings.DatabaseFunctions.*;
 public class MySQLBackend {
-    public static String ConnectionLocation = "jdbc:mysql://localhost:3306/monoclmain";
-    public static String UserName ="JavaConnection";
-    public static String Password ="JavaPassword";
-    public static String Driver = "com.mysql.jdbc.Driver";
-    public static ArrayList QuestionIDArray = new ArrayList();
-    public static ArrayList<TableItem> WordAndCounts = new ArrayList<TableItem>();
     public static void main(String[] args) {
         /**order of operation:
         Connects to DB
@@ -23,103 +11,73 @@ public class MySQLBackend {
         Compares last answe rfor question to last update to that questions file - creates file if one not present
         Gets all the answers for that question
         does the keyword procedure*/
-        try{            
-        Connection ConnectionFunction = DriverManager.getConnection(ConnectionLocation,UserName,Password);
-        Statement statement = ConnectionFunction.createStatement();
-        ResultSet QuestionIDResults = statement.executeQuery("SELECT DISTINCT QuestionID FROM answertable");
+        try{
+        ResultSet QuestionIDResults = SearchQueryReturn("SELECT DISTINCT QuestionID FROM answertable");
         int counter =0;
         while(QuestionIDResults.next()){            
             System.out.println("QuestionID:"+QuestionIDResults.getString(counter));
             //statements to gather the results;
             String QuestionIDString = QuestionIDResults.getString(counter);
-            ResultSet AnswerReturnResults = statement.executeQuery("SELECT AnswerText FROM answertable WHERE QuestionID ="+QuestionIDString);
+            ResultSet AnswerReturnResults = SearchQueryReturn("SELECT AnswerText FROM answertable WHERE QuestionID ="+QuestionIDString);
             ArrayList<String> AnswerArrayList = new ArrayList<String>();
             while(AnswerReturnResults.next()){
                 AnswerArrayList.add(AnswerReturnResults.getString("AnswerText"));
             }
             //funcction to prepare the output file with the string to output;
-            if(PushToFile(QuestionIDResults.getString(counter),OutputStringCreator(QuestionIDString,AnswerArrayList))){      
-                ConnectionFunction.close();
-                System.out.println("Congratulations");
-                }
-            else{
-                System.err.println("Push to file didn't work");
+            if(WordSplitTablePopulator(QuestionIDString,AnswerArrayList) == true){
+                System.out.println("True");
             }
-            counter++;
+            else{
+                System.err.println("False");
+            }
         }
         }             
         catch(Exception FalseSQLQueryResults){
             FalseSQLQueryResults.printStackTrace();
         }
     }
+     
     
     
-    
-    public static boolean PushToFile(String QuestionID,String OutputToPush){
-        String QustionIDPrepatoryString;
-        String DirectoryString = "..\\MonoclAssetFiles\\"+(QuestionID)+".txt";
-        File FileAtDirectory = new File(DirectoryString);
+    public static boolean WordSplitTablePopulator(String QuestionID,ArrayList<String> AnswerArray){
         try{
-            Files.deleteIfExists(Paths.get(DirectoryString));
-            //file already exists
-            //more efficent to just delete the file rather than  
-            //try appending it and checcking if  the reuslts are the same
-            FileAtDirectory.delete();
+        if(IsTablePresent(QuestionID).next()){
+            ClearTable(QuestionID);
+            System.out.println("Table Replaced");
+            
         }
-        catch(Exception FileDeleteIfExistsExemption){
-            //file does not exist - does not require to be deleted;
-           FileDeleteIfExistsExemption.printStackTrace();
-           return false;
+        else{
+            if(CreateTable(QuestionID)){
+                System.out.println("Table Now  exists");
+            };
            
         }
-        try{
-            FileWriter FileWriter = new FileWriter(FileAtDirectory);
-            FileWriter.write(OutputToPush);
-            FileWriter.close();
-            return true;
-        }
-        catch(Exception FileWriterException){
-                FileWriterException.printStackTrace();
-                return false;
-        }
-        
-        
-    }
-    public static String OutputStringCreator(String QuestionID){
-        String OutputString = "QuestionID :"+"\n";
-        OutputString = OutputString + WordAndCounts.get(0).HeaderCreator();
-        for(int i=0;i<WordAndCounts.size();i++){
-            OutputString=OutputString+WordAndCounts.get(i).TableToPrintReturn();
-        }
-        return OutputString;
-        
-        
-    }
-    public static void WordSplitTablePopulator(String QuestionID,ArrayList<String> AnswerArray){
-        for(int i=0;i<AnswerArray.size();i++){//split by answer
-            String[] ArraySplit =AnswerArray.get(i).split(" ");
-            ArrayList ArraySplitList = new ArrayList(Arrays.asList(ArraySplit));
-            for(int x=0;x<ArraySplitList.size();x++){//split by word
-                boolean ispresent= false;
-                boolean forloopcomplete=false;
-               for(int y=0;y<WordAndCounts.size();y++){//checks each word in there
-                if(WordAndCounts.get(y).getWord() == ArraySplitList.get(x)){
-                   WordAndCounts.get(x).CountIncreaser();
-                   ispresent=true;
-                   forloopcomplete=true;
-                   break;
+        for(int i=0;i<AnswerArray.size();i++){
+            String[] SplitByWord=AnswerArray.get(i).split(""); 
+            for (String Word : SplitByWord) {
+                String QueryToSend = "INSERT INTO "+QuestionID+"(MainWord) VALUES ("+Word+");";
+                if(InsertData(QuestionID,QueryToSend)){
+                    System.out.println("Query was succesful");
+                    return true;
                 }
                 else{
-                    forloopcomplete=true;
-                    ispresent=false;
+                    System.err.println("Query Unsuccsesful");
+                    return false;
                 }
-               }
-               if(ispresent == false && forloopcomplete==true){
-                   WordAndCounts.add(new TableItem(ArraySplitList.get(x).toString(),1));
-               }
             }
-       }
-    }
+        }
+        }
+        catch(Exception TablePresent){
+        TablePresent.printStackTrace();   
+        return false;
+        }
+        System.err.println("Error line 74");
+        return false;
+                
+    
+        
+    
+}
     
 }
         
