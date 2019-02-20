120d81c4f3edf236adef910aab988b2aff1a8371
MonocledMajesticProductions <hpd12@live.co.uk>
2019-02-20 16:56:21 +0000

    Update Sort.java

--- MySQLBackendDealings/src/mysqlbackenddealings/Sort.java
+++ MySQLBackendDealings/src/mysqlbackenddealings/Sort.java
@@ -3,44 +3,57 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Sort {
-    public static void QuickSorting(ArrayList<Integer> List,int Low,int High){//0 and size i think
+    public static ArrayList QuickSorting(ArrayList<Integer> List,int Low,int High){
         int OriginalLow=0;
         int OriginalHigh=List.size()-1;
-        int LeftPointer=0;
-        int RightPointer=List.size()-1;
+        int LeftPointer=Low;
+        int RightPointer=High;
+        System.out.println("Recursive Call");
         if(List.size()<2){
-            //break;
+            return List;
         }
         else{
             Random random=new Random();
-            int pivot=random.nextInt(List.size()-1)+0;
-            System.out.println("Pivot"+List.get(pivot));
+            int pivot=List.get(random.nextInt((List.size()-1))+0);
             
-            while(LeftPointer!=RightPointer){
-               // System.out.println("Pivot :"+List.get(pivot));
+            
+            while(LeftPointer<=RightPointer){
+//                System.out.println("Pivot :"+List.get(pivot));
                 //System.out.println("LeftPointer+1 :"+List.get(LeftPointer+1));
-                System.out.println("LeftPointer "+LeftPointer);
-                System.out.println("LeftPointer get "+List.get(LeftPointer));
-                while(List.get(LeftPointer)>=List.get(pivot)){
-                   System.out.println("LeftPointer "+LeftPointer);
-                    LeftPointer=LeftPointer+1;
-                }
-               // System.out.println("RightPointer+1:"+List.get(RightPointer-1));
-                while(List.get(RightPointer-1)<=List.get(pivot)){
-                    System.out.println("RightPointer "+RightPointer);
-                    RightPointer=RightPointer-1;
-                }
                // System.out.println("LeftPointer "+LeftPointer);
-               // System.out.println("RightPointer "+RightPointer);
-                int TempStorage=List.get(RightPointer);
-                List.set(RightPointer,List.get(LeftPointer));
-                List.set(LeftPointer,TempStorage);
+                //System.out.println("LeftPointer get "+List.get(LeftPointer));
+                while(List.get(LeftPointer)<pivot){
+                   System.out.println("LeftPointer "+LeftPointer);
+                    LeftPointer++;
+                }
+               
+                while(List.get(RightPointer)>pivot){
+                   System.out.println(" 57 RightPointer "+RightPointer+ "Pivot : " +pivot);
+                    RightPointer--;
+                }
+              //  System.out.println("LeftPointer "+LeftPointer);
+                //System.out.println("RightPointer "+RightPointer);
+                int TempStorage=List.get(LeftPointer);
+                if(LeftPointer<=RightPointer){
+                List.set(LeftPointer,List.get(RightPointer));
+                List.set(RightPointer,TempStorage);
+                 LeftPointer++;
+                 RightPointer--;
+                }
+                if(Low<RightPointer){
+                    QuickSorting(List,Low,RightPointer);
+                }
+                if(LeftPointer<High){
+                    QuickSorting(List,LeftPointer,High);
+                }
+                    
+                }
             }
-            System.out.println(List);
+            return List;
                         
         }
      
 }
-}
+
 
 
