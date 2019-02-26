2048b63289f5e48a8df91c48ebebee7f275e557e
MonocledMajesticProductions <hpd12@live.co.uk>
2019-02-26 18:24:48 +0000

    Create MySQLConnectionFile.php

---
+++ MySQLConnectionFile.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost:3306";
+$account = "PHPConnection2";
+$dbname = "monoclmain";
+$password="PHPPassword12";
+$ConnectionFunction = mysqli_connect($servername, $account, $password, $dbname);
+?>
